349df6d98a035391d12537ff11644c668fc6296e
Kenneth Cannon <KCannon@ashleyfurniture.com>
2020-01-20 11:03:36 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace CardFlip
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cardBackPictureBox = new System.Windows.Forms.PictureBox();
+            this.cardFacePictureBox = new System.Windows.Forms.PictureBox();
+            this.showBackButton = new System.Windows.Forms.Button();
+            this.showFaceButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.cardBackPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardFacePictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cardBackPictureBox
+            // 
+            this.cardBackPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("cardBackPictureBox.Image")));
+            this.cardBackPictureBox.Location = new System.Drawing.Point(187, 72);
+            this.cardBackPictureBox.Name = "cardBackPictureBox";
+            this.cardBackPictureBox.Size = new System.Drawing.Size(100, 140);
+            this.cardBackPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.cardBackPictureBox.TabIndex = 0;
+            this.cardBackPictureBox.TabStop = false;
+            this.cardBackPictureBox.Visible = false;
+            // 
+            // cardFacePictureBox
+            // 
+            this.cardFacePictureBox.Image = ((System.Drawing.Image)(resources.GetObject("cardFacePictureBox.Image")));
+            this.cardFacePictureBox.Location = new System.Drawing.Point(431, 72);
+            this.cardFacePictureBox.Name = "cardFacePictureBox";
+            this.cardFacePictureBox.Size = new System.Drawing.Size(100, 140);
+            this.cardFacePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.cardFacePictureBox.TabIndex = 1;
+            this.cardFacePictureBox.TabStop = false;
+            this.cardFacePictureBox.Visible = false;
+            // 
+            // showBackButton
+            // 
+            this.showBackButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.showBackButton.Location = new System.Drawing.Point(187, 285);
+            this.showBackButton.Name = "showBackButton";
+            this.showBackButton.Size = new System.Drawing.Size(100, 56);
+            this.showBackButton.TabIndex = 2;
+            this.showBackButton.Text = "Show the Card Back";
+            this.showBackButton.UseVisualStyleBackColor = true;
+            this.showBackButton.Click += new System.EventHandler(this.showBackButton_Click);
+            // 
+            // showFaceButton
+            // 
+            this.showFaceButton.Location = new System.Drawing.Point(431, 285);
+            this.showFaceButton.Name = "showFaceButton";
+            this.showFaceButton.Size = new System.Drawing.Size(100, 56);
+            this.showFaceButton.TabIndex = 3;
+            this.showFaceButton.Text = "Show the Face Card";
+            this.showFaceButton.UseVisualStyleBackColor = true;
+            this.showFaceButton.Click += new System.EventHandler(this.showFaceButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.showFaceButton);
+            this.Controls.Add(this.showBackButton);
+            this.Controls.Add(this.cardFacePictureBox);
+            this.Controls.Add(this.cardBackPictureBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.cardBackPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardFacePictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox cardBackPictureBox;
+        private System.Windows.Forms.PictureBox cardFacePictureBox;
+        private System.Windows.Forms.Button showBackButton;
+        private System.Windows.Forms.Button showFaceButton;
+    }
+}
+
